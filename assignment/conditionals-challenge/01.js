6dcd5dd390fcc0816aa9a6d1bebf8197d98acbd3
navaleganesh <navale.ganesh1@gmail.com>
2025-03-01 16:59:29 +0530

    Conditionals Challenge

---
+++ assignment/conditionals-challenge/01.js
@@ -0,0 +1,11 @@
+// You just need to implement the checkEvenOdd function
+function checkEvenOdd(num) {
+    // Return "Even" if num is even, otherwise return "Odd"
+    if(num % 2 === 0){
+      return "Even"
+    } else {
+      return "Odd"
+    }
+  }
+  console.log(checkEvenOdd(5));
+  
