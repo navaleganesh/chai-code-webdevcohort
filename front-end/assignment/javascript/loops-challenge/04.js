cdeb9c41fe2915faf9e435aee3773a6b1894bf1d
navaleganesh <navale.ganesh1@gmail.com>
2025-03-03 05:07:44 +0530

    workingDays

---
+++ front-end/assignment/javascript/loops-challenge/04.js
@@ -0,0 +1,20 @@
+// You just need to implement the workingDays function
+let days = ["Monday","Tuesday","Saturday"]
+
+function workingDays(days) {
+    // your code here
+    
+    return days.filter((item,index) => {
+    
+        if(item !== "Sunday" && item !== "Saturday"){
+            return item
+        } 
+        
+     }
+    )
+    
+  }
+
+
+const filterDay = workingDays(days)
+console.log(filterDay.length);
